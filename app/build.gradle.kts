978d46af57a729c3b621e8e26c392f680cbc3e4d
abdsalama <abdsalama2020@gmail.com>
2025-04-26 16:43:13 +0300

    fix Gradle and update to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id ("kotlin-kapt")
     id("com.google.devtools.ksp")
 }
 
@@ -32,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -53,8 +52,7 @@ android {
 }
 dependencies {
 
-
-    implementation ("androidx.compose.material:material-icons-extended:<نسخة_الـ_compose>")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.31.1-alpha")
     implementation(libs.androidx.core.ktx)
@@ -107,11 +105,11 @@ dependencies {
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.51")
-    kapt("com.google.dagger:hilt-android-compiler:2.51")
+    ksp("com.google.dagger:hilt-android-compiler:2.51")
 
     // Hilt for WorkManager integration
     implementation("androidx.hilt:hilt-work:1.2.0")
-    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    ksp("androidx.hilt:hilt-compiler:1.2.0")
 
     // WorkManager
     implementation("androidx.work:work-runtime-ktx:2.9.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,16 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
+
+// Configure Java toolchain for all projects
+allprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+    
+    // The JavaCompile release option is not supported in Android
+    // We'll rely on the sourceCompatibility and targetCompatibility 
+    // settings in app/build.gradle.kts instead
+}
